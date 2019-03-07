d9404129ec7ae78317e589c60ab7f24c42ffd28d
ApoPsallas <a.psallas@thebet.co>
2019-03-07 17:07:56 +0200

    check if file in arg exists

--- HelloWorldApp.go
+++ HelloWorldApp.go
@@ -5,6 +5,11 @@ import (
 	"os"
 )
 
+var (
+	fileInfo *os.FileInfo
+	err      error
+)
+
 func getArgument() string {
 	if len(os.Args) > 1 {
 		return os.Args[1]
@@ -12,7 +17,19 @@ func getArgument() string {
 		return "no args"
 	}
 }
+func fileExists(filename string) bool {
+	// Stat returns file info. It will return
+	// an error if there is no file.
+	fileInfo, err := os.Stat(filename)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return false
+		}
+	}
+	_ = fileInfo
+	return true
+}
 func main() {
-	fmt.Println(getArgument())
+	fmt.Println(fileExists(getArgument()))
 
 }
