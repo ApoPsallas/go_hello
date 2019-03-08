20020cc572b8b40ee108c3ff8919438d048532d6
Constantinos Christofilos <k.christofilos@thebeat.co>
2019-03-08 13:36:43 +0200

    Refactor

--- HelloWorldApp.go
+++ HelloWorldApp.go
@@ -1,45 +1,79 @@
 package main
 
 import (
+	"encoding/base64"
+	"errors"
 	"fmt"
+	"io/ioutil"
 	"os"
 )
 
-var (
-	fileInfo *os.FileInfo
-	err      error
+const (
+	errNoError int = 0 + iota
+	errArgumentMissing
+	errFileNotExist
+	errFileNotReadable
 )
 
-func getArgument() string {
-	if len(os.Args) > 1 {
-		return os.Args[1]
-	} else {
-		return "no args"
+func main() {
+	fileName, err := getArgument()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(errArgumentMissing)
 	}
+
+	fileExists := fileExists(fileName)
+	if !fileExists {
+		fmt.Printf("File '%s' does not exist\n", fileName)
+		os.Exit(errFileNotExist)
+	}
+
+	fileIsReadable := fileIsReadable(fileName)
+	if !fileIsReadable {
+		fmt.Printf("File '%s' is not readable\n", fileName)
+		os.Exit(errFileNotReadable)
+	}
+
+	encoded := encodeFile(fileName)
+	fmt.Println(encoded)
+
+	os.Exit(errNoError)
+
 }
+
+func encodeFile(filename string) string {
+	fileContent, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return ""
+	}
+
+	return base64.StdEncoding.EncodeToString(fileContent)
+}
+
+func getArgument() (string, error) {
+	if len(os.Args) > 1 {
+		return os.Args[1], nil
+	}
+
+	return "", errors.New("No argument was defined")
+}
+
 func fileExists(filename string) bool {
 	// Stat returns file info. It will return
 	// an error if there is no file.
-	fileInfo, err := os.Stat(filename)
-	if err != nil {
-		if os.IsNotExist(err) {
-			return false
-		}
+	_, err := os.Stat(filename)
+	if err != nil && os.IsNotExist(err) {
+		return false
 	}
-	_ = fileInfo //temporary
-	return true
-}
-func fileReadPermition(filename string) bool {
-	file, err := os.OpenFile(filename, os.O_RDONLY, 0666)
-	if err != nil {
-		if os.IsPermission(err) {
-			return false
-		}
-	}
-	_ = file //temporary
-	return true
-}
-func main() {
-	fmt.Println(fileReadPermition(getArgument())) //check file permition from argument
 
+	return true
+}
+
+func fileIsReadable(filename string) bool {
+	_, err := os.OpenFile(filename, os.O_RDONLY, 0666)
+	if err != nil && os.IsPermission(err) {
+		return false
+	}
+
+	return true
 }
